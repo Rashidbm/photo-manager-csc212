937631d941b27aca733a91f666e6d8bd5866abef
Saud <s3od@outlook.sa>
2025-05-03 21:17:46 +0300

    Removed the import from Album class

--- src/Album.java
+++ src/Album.java
@@ -1,16 +1,14 @@
-import java.util.LinkedList;
-
 public class Album {
     private String name;
     private String condition;
     private PhotoManager manager;
-    private int nbComps;
-
+    private int nbComps; 
 
     public Album(String name, String condition, PhotoManager manager) {
         this.name = name;
         this.condition = condition;
         this.manager = manager;
+        this.nbComps = 0;
     }
 
     // name getter
@@ -26,59 +24,53 @@ public class Album {
     // manager getter
     public PhotoManager getManager() {
         return manager;
-        
-    }    
- 
+    }
+
     public LinkedList<Photo> getPhotos() {
-        int nbComps = 0;
+        nbComps = 0; 
+
         LinkedList<Photo> result = new LinkedList<>();
-        
+
         if (condition == null || condition.isEmpty()) 
             return manager.getPhotos();
-        
+
         String[] tags = condition.split(" AND ");
-        
+
         manager.getPhotos().findFirst();
         do {
             Photo p = manager.getPhotos().retrieve();
             boolean match = true;
-            
+
             for (String tag : tags) {
                 boolean found = false;
                 p.getTags().findFirst();
 
-                
                 do {
                     nbComps++;
-                    if (p.getTags().retrieve().equals(tag)) { 
-                        found = true; 
-                        break; 
+                    if (p.getTags().retrieve().equals(tag)) {
+                        found = true;
+                        break;
                     }
                     p.getTags().findNext();
-                }
-                while (!p.getTags().last());
+                } while (!p.getTags().last());
 
-
-
-                if (!found) { 
-                    match = false; 
-                    break; 
+                if (!found) {
+                    match = false;
+                    break;
                 }
             }
-                        if (match) 
-                result.insert(p);
-            
-            manager.getPhotos().findNext();
-        }
-        while (!manager.getPhotos().last()) ;
 
+            if (match)
+                result.insert(p);
+
+            manager.getPhotos().findNext();
+        } while (!manager.getPhotos().last());
 
         return result;
-
     }
 
-    public int getNbComps() { 
-        return nbComps; 
+    
+    public int getNbComps() {
+        return nbComps;
     }
-
 }
